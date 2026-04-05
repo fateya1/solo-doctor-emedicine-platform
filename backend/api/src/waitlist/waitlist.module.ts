cc07040ddbdc7a7d3e0b8fa430eb8fe7381f77f7
RMjomba <youremail@example.com>
2026-04-05 06:27:48 +0300

    fix: add EmailModule to WaitlistModule

--- backend/api/src/waitlist/waitlist.module.ts
+++ backend/api/src/waitlist/waitlist.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { WaitlistController } from "./waitlist.controller";
 import { WaitlistService } from "./waitlist.service";
 import { PrismaModule } from "../prisma/prisma.module";
+import { EmailModule } from "../email/email.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EmailModule],
   controllers: [WaitlistController],
   providers: [WaitlistService],
   exports: [WaitlistService],
